dda2e2f8e24ed935f9d7c413fa75e7e545bb293d
Iliyass Zamouri <zamouri@mobiblanc.com>
2024-02-20 17:36:03 +0100

    Cleanup

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Initialize and start PostgreSQL
-service postgresql start
-
-# Optionally, initialize the database here (e.g., create tables)
-
-# Start your Java application
-java -jar /app/target/*.jar
--- mvn-17.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set JAVA_HOME to JDK 17
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home
-
-# Invoke Maven with all arguments passed to the script
-mvn "$@"
